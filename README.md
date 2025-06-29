ff5fd8bc9b45acda2861a0f4edc992ada25220e3
Tom Greene <CosmosBI@outlook.com>
2025-06-29 17:54:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### https://www.youtube.com/watch?v=mKmxc8TcWQ8
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const corsOptions = {
+  origin: ['http://localhost:5173'], // Adjust this to your frontend's URL
+  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' //,
+  // credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.get('/api', (req, res) => {
+  res.json({ fruits: ["apple", "banana", "orange"] });
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
